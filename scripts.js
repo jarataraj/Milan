be902ad53eff9895c766f142732da98379b1059f
jarataraj <32778952+jarataraj@users.noreply.github.com>
2018-02-14 11:57:06 -0500

    Add no hover effect on mobile

--- scripts.js
+++ scripts.js
@@ -18,37 +18,72 @@ $(document).ready(function(){
              $('nav').removeClass('fixed');
          } 
     });
-    
-});
 
-var sections = $('section'),
-    nav = $('nav'),
-    nav_height = nav.outerHeight();
+    var sections = $('section'),
+        nav = $('nav'),
+        nav_height = nav.outerHeight();
 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-  
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height - 20,
-        bottom = top + $(this).outerHeight();
+    $(window).on('scroll', function () {
+      var cur_pos = $(this).scrollTop();
+
+      sections.each(function() {
+        var top = $(this).offset().top - nav_height - 20,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+          nav.find('li').removeClass('active');
+          sections.removeClass('active');
+
+          $(this).addClass('active');
+          nav.find('a[href="#'+$(this).attr('id')+'"] li').addClass('active');
+        }
+      });
+    });
+
+    nav.find('a').on('click', function () {
+      var $el = $(this)
+        , id = $el.attr('href');
+
+      $('html, body').animate({
+        scrollTop: $(id).offset().top - nav_height + 1
+      }, 500);
+
+      return false;
+    });
     
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('li').removeClass('active');
-      sections.removeClass('active');
-      
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"] li').addClass('active');
+    function watchForHover() {
+        var hasHoverClass = false;
+        var container = document.body;
+        var lastTouchTime = 0;
+
+        function enableHover() {
+            // filter emulated events coming from touch events
+            if (new Date() - lastTouchTime < 500) return;
+            if (hasHoverClass) return;
+
+            container.className += ' hasHover';
+            hasHoverClass = true;
+        }
+
+        function disableHover() {
+            if (!hasHoverClass) return;
+
+            container.className = container.className.replace(' hasHover', '');
+            hasHoverClass = false;
+        }
+
+        function updateLastTouchTime() {
+            lastTouchTime = new Date();
+        }
+
+        document.addEventListener('touchstart', updateLastTouchTime, true);
+        document.addEventListener('touchstart', disableHover, true);
+        document.addEventListener('mousemove', enableHover, true);
+
+        enableHover();
     }
-  });
+
+    watchForHover();
+    
 });
 
-nav.find('a').on('click', function () {
-  var $el = $(this)
-    , id = $el.attr('href');
-  
-  $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height + 1
-  }, 500);
-  
-  return false;
-});
